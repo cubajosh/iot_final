51e08480646b7b3b2ebc8053ec70582838d3b49a
cubajosh <cubajosh3@gmail.com>
2022-12-05 18:42:46 -0500

    The emulator is complete and fully functional

---
+++ IOTFinalProject/Emulator.py
@@ -0,0 +1,65 @@
+import pymongo
+import datetime as dt
+import random
+from time import sleep
+
+client = pymongo.MongoClient("mongodb+srv://balls:balls@tempproject.zkvxtwe.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+
+sensorOn = True
+loopNum = 0
+
+if 'weather' not in db.list_collection_names():
+    db.create_collection('weather',
+
+                         timeseries={'timeField': 'timestamp', 'metaField': 'id', 'granularity': 'seconds'}
+
+                         )
+    db.weather.insert_one({
+
+        'timestamp': dt.datetime.now(),
+        'id': 1,
+        'temp': 40,
+        'humidity': 22,
+        'light levels': 150
+    })
+
+if __name__ == "__main__":
+    while sensorOn:
+        randomTemp = random.randint(0, 100)
+        humidity = random.randint(0, 100)
+        lightLevel = random.randint(0, 100)
+
+        db.weather.insert_one({
+
+            'timestamp': dt.datetime.now(),
+            'id': 1,
+            'temp': randomTemp,
+            'humidity': humidity,
+            'light levels': lightLevel
+        })
+        db.weather.insert_one({
+
+            'timestamp': dt.datetime.now(),
+            'id': 2,
+            'temp': randomTemp,
+            'humidity': humidity,
+            'light levels': lightLevel
+        })
+        db.weather.insert_one({
+
+            'timestamp': dt.datetime.now(),
+            'id': 3,
+            'temp': randomTemp,
+            'humidity': humidity,
+            'light levels': lightLevel
+        })
+        db.weather.insert_one({
+
+            'timestamp': dt.datetime.now(),
+            'id': 4,
+            'temp': randomTemp,
+            'humidity': humidity,
+            'light levels': lightLevel
+        })
+        sleep(2)
--- IOTFinalProject/main.py
+++ IOTFinalProject/main.py
@@ -1,72 +1,19 @@
-import pymongo as pymongo
 from flask import Flask, request, jsonify
 import datetime as dt
+import Emulator as em
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-client = pymongo.MongoClient("mongodb+srv://balls:balls@tempproject.zkvxtwe.mongodb.net/?retryWrites=true&w=majority")
-db = client.test
-
-if 'weather' not in db.list_collection_names():
-    db.create_collection('weather',
-
-                         timeseries={'timeField': 'timestamp', 'metaField': 'id', 'granularity': 'seconds'}
-
-                         )
-# dummy data to see if the conection works#
-db.weather.insert_one({
-
-    'timestamp': dt.datetime.now(),
-    'id': 1,
-    'temp': 32.1,
-    'humidity': 87.1,
-    'light levels': 150
-})
-
-
-def getTimeStamp():
-    return dt.datetime.today().replace(microsecond=0)
-
-
-@app.route("/temps/all")
+@app.route("/temps")
 def getAll():
-
-    start = request.args.get("start")
-    end = request.args.get("end")
-
-    query = db.collection.find()
+    query = em.db.collection.find()
     tempList = {}
 
     for x in query:
         tempList = {'temps': x}
 
-        if start is None and end is not None:
-            try:
-                end = dt.datetime.strptime(end, "%Y-%m-%dT%H:%M:%S")
-            except Exception as e:
-                return {"error": "timestamp not following format %Y-%m-%dT%H:%M:%S"}, 400
-
-            tempList.update({"timestamp": {"$lte": end}})
-
-        elif end is None and start is not None:
-            try:
-                start = dt.datetime.strptime(start, "%Y-%m-%dT%H:%M:%S")
-            except Exception as e:
-                return {"error": "timestamp not following format %Y-%m-%dT%H:%M:%S"}, 400
-
-            tempList.update({"timestamp": {"$gte": start}})
-        elif start is not None and end is not None:
-            try:
-                start = dt.datetime.strptime(start, "%Y-%m-%dT%H:%M:%S")
-                end = dt.datetime.strptime(end, "%Y-%m-%dT%H:%M:%S")
-
-            except Exception as e:
-                return {"error": "timestamp not following format %Y-%m-%dT%H:%M:%S"}, 400
-
-            tempList.update({"timestamp": {"$gte": start, "$lte": end}})
-
-    data = list(db.weather.aggregate([
+    data = list(em.db.weather.aggregate([
         {
             '$match': tempList
         }, {
@@ -127,7 +74,7 @@ def getTempById(tempId):
 
         query.update({"timestamp": {"$gte": start, "$lte": end}})
 
-    data = list(db.weather.aggregate([
+    data = list(em.db.weather.aggregate([
         {
             '$match': query
         }, {
@@ -166,7 +113,7 @@ def getTempById(tempId):
 
         return data
     else:
-        return {"error": "id not found, the only Id we have is 1 (for testing)"}, 404
+        return {"error": "Id does not exist"}, 404
 
 
 if __name__ == "__main__":
